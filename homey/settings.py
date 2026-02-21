9e6890d8687e8b586e28f241a0981353510a7df5
TekDiverse <108485884+ChijiokeDivine@users.noreply.github.com>
2026-02-21 19:01:10 +0100

    ...

--- homey/settings.py
+++ homey/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["https://homey-fqn5.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
